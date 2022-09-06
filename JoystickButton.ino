aa412d69e09eba03a172b71cae8d57b0e300d984
Enis Bukalo <bukaloenis@gmail.com>
2022-09-06 14:59:38 -0400

    Update

--- JoystickButton.ino
+++ JoystickButton.ino
@@ -1,24 +1,29 @@
 #include <Joystick.h>
+#include <XInput.h>
 
+const int pinAmount = 4;
 Joystick_ Joystick(0x04, JOYSTICK_TYPE_JOYSTICK,
-  1, 0,                 // Button Count, Hat Switch Count 
-  false, false, false,  // X, Y, Z Axis
+  4, 0,                 // Button Count, Hat Switch Count 
+  true, true, true,     // X, Y, Z Axis
   false, false, false,  // Rx, Ry, Rz Axis    
   false, false,         // Rudder, Throttle
   false, false, false); // Accelerator, Brake Steering
 
 void setup() {
 
-  int pins[1] = {2};
+  int pins[pinAmount] = {2,3,4,5};
 
   // Initialize Button Pins
   pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
 
   // Initialize Joystick Library
   Joystick.begin();
 
   // Lets Release All The Buttons Just In Case They Init High.
-  for (int index = 0; index < 1; index++){
+  for (int index = 0; index < pinAmount; index++){
     Joystick.releaseButton(pins[index]);
   }
 }
@@ -27,12 +32,12 @@ void setup() {
 const int pinToButtonMap = 2;
 
 // Last state of the button
-int lastButtonState[1] = {0};
+int lastButtonState[pinAmount] = {0, 0, 0, 0};
 
 void loop() {
 
   // Read pin values
-  for (int index = 0; index < 1; index++)
+  for (int index = 0; index < pinAmount; index++)
   {
     int currentButtonState = !digitalRead(index + pinToButtonMap);
     if (currentButtonState != lastButtonState[index])
--- JoystickButton/JoystickButton.ino
+++ JoystickButton/JoystickButton.ino
@@ -1,37 +1,29 @@
-// Simple example application that shows how to read four Arduino
-// digital pins and map them to the USB Joystick library.
-//
-// Ground digital pins 9, 10, 11, and 12 to press the joystick 
-// buttons 0, 1, 2, and 3.
-//
-// NOTE: This sketch file is for use with Arduino Leonardo and
-//       Arduino Micro only.
-//
-// by Matthew Heironimus
-// 2015-11-20
-//--------------------------------------------------------------------
-
 #include <Joystick.h>
+#include <XInput.h>
 
+const int pinAmount = 4;
 Joystick_ Joystick(0x04, JOYSTICK_TYPE_JOYSTICK,
-  1, 0,                 // Button Count, Hat Switch Count 
-  false, false, false,  // X, Y, Z Axis
+  4, 0,                 // Button Count, Hat Switch Count 
+  true, true, true,  // X, Y, Z Axis
   false, false, false,  // Rx, Ry, Rz Axis    
   false, false,         // Rudder, Throttle
   false, false, false); // Accelerator, Brake Steering
 
 void setup() {
 
-  int pins[1] = {2};
+  int pins[pinAmount] = {2,3,4,5};
 
   // Initialize Button Pins
   pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
 
   // Initialize Joystick Library
   Joystick.begin();
 
   // Lets Release All The Buttons Just In Case They Init High.
-  for (int index = 0; index < 1; index++){
+  for (int index = 0; index < pinAmount; index++){
     Joystick.releaseButton(pins[index]);
   }
 }
@@ -40,12 +32,12 @@ void setup() {
 const int pinToButtonMap = 2;
 
 // Last state of the button
-int lastButtonState[1] = {0};
+int lastButtonState[pinAmount] = {0, 0, 0, 0};
 
 void loop() {
 
   // Read pin values
-  for (int index = 0; index < 1; index++)
+  for (int index = 0; index < pinAmount; index++)
   {
     int currentButtonState = !digitalRead(index + pinToButtonMap);
     if (currentButtonState != lastButtonState[index])
